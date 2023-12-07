c865eb478b4bc24c81ec319b19b9a000d860d349
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-12-07 15:10:05 +0600

    fremer motion

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
 import { motion } from "framer-motion";
+import {fadeIn} from "../../variants"
 const Banner = () => {
     const [text] = useTypewriter({
         words: ['Rashadul Islam'],
@@ -10,23 +11,24 @@ const Banner = () => {
     <div>
       <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse gap-10 lg:mx-10">
-          <div className="flex-1 lg:ml-48">
-          <motion.div
-      className="box"
-      /**
-       * Setting the initial keyframe to "null" will use
-       * the current value to allow for interruptable keyframes.
-       */
-      whileHover={{ scale: [null, 1.5, 1.4] }}
-      transition={{ duration: 0.3 }}
-    />
+          <motion.div 
+           variants={fadeIn("left",0.1)}
+           initial="hidden"
+           whileInView={"show"}
+           viewport={{ once:false,amount:0.7 }}
+          className="flex-1 lg:ml-48">
             <img
             
               src="https://i.ibb.co/Cvhzv2g/login.jpg"
               className="max-w-sm rounded-lg shadow-2xl"
             />
-          </div>
-          <div className="flex-1 ">
+          </motion.div>
+          <motion.div 
+           variants={fadeIn("right",0.1)}
+           initial="hidden"
+           whileInView={"show"}
+           viewport={{ once:false,amount:0.7 }}
+          className="flex-1 ml-20">
             <h1 className="text-5xl font-bold">Start your Journey</h1>
             <div>
               <div className="App text-5xl font-bold">
@@ -39,7 +41,7 @@ const Banner = () => {
               error-free websites with 100% client satisfaction
             </p>
             <button className="btn btn-primary">Contact me</button>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
