05e60103734a1937246f1eb4e68308e85c534685
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-12-07 16:18:57 +0600

    Particles added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,31 +1,45 @@
 import AnimatedCursor from "react-animated-cursor";
 import Banner from "../../components/Banner/Banner";
+import Particle from "../../components/Particle";
 
 
 const Home = () => {
     return (
         <div>
+          <Particle></Particle>
              <AnimatedCursor 
              
              innerSize={8}
-             outerSize={8}
-             color='193, 11, 111'
-             outerAlpha={0.2}
-             innerScale={0.7}
-             outerScale={5}
-             clickables={[
-                'a',
-                'input[type="text"]',
-                'input[type="email"]',
-                'input[type="number"]',
-                'input[type="submit"]',
-                'input[type="image"]',
-                'label[for]',
-                'select',
-                'textarea',
-                'button',
-                '.link'
-              ]}
+      outerSize={8}
+      color='193, 11, 111'
+      outerAlpha={0.2}
+      innerScale={0.7}
+      outerScale={5}
+      clickables={[
+        'a',
+        'input[type="text"]',
+        'input[type="email"]',
+        'input[type="number"]',
+        'input[type="submit"]',
+        'input[type="image"]',
+        'label[for]',
+        'select',
+        'textarea',
+        'button',
+        '.link',
+        {
+          target: '.custom',
+          options: {
+            innerSize: 12,
+            outerSize: 12,
+            color: '255, 255, 255',
+            outerAlpha: 0.3,
+            innerScale: 0.7,
+            outerScale: 5
+          }
+        }
+      ]}
+             
              />
             <Banner></Banner>
         </div>
