f52f3a0f3fb462b98814354b9d5868c84c3f3309
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-12-10 17:58:57 +0600

    fixed navber implement

--- src/Pages/Rabby/Rabby.jsx
+++ src/Pages/Rabby/Rabby.jsx
@@ -10,7 +10,7 @@ const Rabby = () => {
         <div className="bg-gradient-to-r from-blue-500 to-purple-500 py-10 text-white relative">
            <div>
            <Services></Services>
-           <div className="absolute top-0 right-0">
+           <div className="top-12 right-32 lg:right-0 navbar fixed z-10 bg-opacity-70 bg-black text-white w-[6%] lg:w-[13%] rounded-xl">
            <Nav></Nav>
            </div>
            </div>
--- src/components/Navber/Nav.jsx
+++ src/components/Navber/Nav.jsx
@@ -1,13 +1,26 @@
+import { FaFacebook, FaGithub, FaHome, FaLinkedin } from "react-icons/fa";
 
 
 const Nav = () => {
     return (
-        <div>
-           <ul>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
-            <li>Home</li>
+        <div className="">
+           <ul className="flex flex-col w-full text-2xl">
+            <li className="flex items-center gap-4 my-3">
+               <FaHome></FaHome>
+                Home
+            </li>
+            <li className="flex items-center gap-4 my-3">
+                <FaFacebook></FaFacebook>
+                Facebook
+            </li>
+            <li className="flex items-center gap-4  my-3">
+               <FaGithub></FaGithub>
+                Github
+            </li>
+            <li className="flex items-center gap-4 my-3">
+                <FaLinkedin></FaLinkedin>
+                FaLinkedin
+            </li>
            </ul>
         </div>
     );
